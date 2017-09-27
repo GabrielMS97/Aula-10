21847bf287b33ebb8b291b633e718dcd4972a15d
ice <ice@lab06m26.nrc.ice.ufjf.br>
2017-09-26 22:22:14 -0300

    Aula 10

---
+++ src/br/ufjf/dcc171/Aula10Tabelas.java
@@ -0,0 +1,15 @@
+package br.ufjf.dcc171;
+
+import javax.swing.JFrame;
+
+public class Aula10Tabelas {
+
+    public static void main(String[] args) {
+        JanelaTabela j = new JanelaTabela();
+        j.setSize(400, 300);
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        j.setLocationRelativeTo(null);
+        j.setVisible(true);
+    }
+
+}
